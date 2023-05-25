41318ecfe647e9f8945c9a6c5008e7e930123147
kimbro97 <rlagudwog@gmail.com>
2023-05-25 17:58:44 +0900

    Add 메인페이지 관리자페이지 분리

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,11 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // 일반 페이지 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
+    // 관리자 페이지 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    { path: '/console', name: 'about', component: () => import('../views/AboutView.vue') }
   ]
 })
 
