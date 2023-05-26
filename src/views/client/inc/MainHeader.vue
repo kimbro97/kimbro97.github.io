babcc4dce1f568c4fd29cc06142ebb4c1e07b521
kimbro97 <rlagudwog@gmail.com>
2023-05-26 22:45:12 +0900

    feat: Add header 검색창 추가 작업

--- src/views/client/inc/MainHeader.vue
+++ src/views/client/inc/MainHeader.vue
@@ -1,20 +1,34 @@
 <template>
     <header class="page-header-wrapper">
-        <div class="page-headerr">
+        <div class="page-header">
             <div class="front-section">
                 <a class="link link-title" href="https://kimbro97.github.io">kimbro97.github.io</a>
             </div>
             <div class="trailing-section">
                 <a class="link">블로그</a>
+                <div class="search-input">
+                    <input class="input-field" type="text">
+                    <div class="icon-container">
+                      <svg-icon class="search-icon" type="mdi" :path="path"></svg-icon>
+                    </div>
+                </div>
             </div>
+
         </div>
     </header>
 </template>
 
 <script >
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiMagnify } from '@mdi/js';
 export default {
     components: {
-
+      SvgIcon
+    },
+    data() {
+        return {
+            path: mdiMagnify
+        }
     },
     computed: {
 
@@ -24,6 +38,41 @@ export default {
 </script>
 
 <style>
+
+    @media (max-width: 770px) {
+        .search-input {
+            display: none;
+        }
+    }
+    @media (min-width: 770px) {
+      .search-input {
+          display: flex;
+          align-items: center;
+          border-bottom: 2px solid black;
+      }
+    }
+
+    .input-field {
+        flex: 1;
+        width: 180px;
+        height: 30px;
+        border: none;
+        background: transparent;
+        outline: none;
+    }
+
+    .icon-container {
+        display: flex;
+        align-items: center;
+        padding-left: 5px; /* 아이콘과 입력란 사이의 간격 조정 */
+    }
+
+    .search-icon {
+        width: 20px;
+        height: 20px;
+        color: #000;
+    }
+
     .page-header-wrapper {
         display: flex;
         justify-content: center;
@@ -39,12 +88,38 @@ export default {
         max-width: 720px;
     }
 
+    .page-header-wrapper .page-header .front-section .link {
+        margin-right: 10px;
+    }
+
+    .page-header-wrapper .page-header .link {
+      color: #000;
+      font-size: 20px;
+      font-weight: 700;
+    }
+
     .page-header-wrapper .page-header .trailing-section {
         align-items: center;
         display: flex;
     }
-    .page-header-wrapper .page-header .link {
-        font-size: 20px;
-        font-weight: 700;
+    .page-header-wrapper .page-header .trailing-section .link {
+        margin-right: 10px;
     }
+
+    .page-header-wrapper .page-header .link {
+      color: #000;
+      font-size: 20px;
+      font-weight: 700;
+    }
+
+    a {
+        outline: none;
+        text-decoration: none;
+    }
+
+    div {
+        display: block;
+    }
+
+
 </style>
